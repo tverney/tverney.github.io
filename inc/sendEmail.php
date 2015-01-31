6a71ab66f671db190974ff4cb6c79f19d555d1ce
Thiago Verney <thiago.verney@gmail.com>
2015-01-31 14:06:09 -0200

    Novas alterações

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'thiago.verney@gmail.com';
 
 
 if($_POST) {
@@ -13,26 +13,26 @@ if($_POST) {
 
    // Check Name
 	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
+		$error['name'] = "Por favor, digite seu nome.";
 	}
 	// Check Email
 	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Please enter a valid email address.";
+		$error['email'] = "Por favor, digite um e-mail válido.";
 	}
 	// Check Message
 	if (strlen($contact_message) < 15) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+		$error['message'] = "Deixe sua mensagem. Deve ter no mínimo de 15 caracteres";
 	}
    // Subject
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+	if ($subject == '') { $subject = "Formulário de contato - CV Thiago Verney"; }
 
 
    // Set Message
-   $message .= "Email from: " . $name . "<br />";
-	$message .= "Email address: " . $email . "<br />";
-   $message .= "Message: <br />";
+   $message .= "E-mail de: " . $name . "<br />";
+   $message .= "Endereço de e-mail: " . $email . "<br />";
+   $message .= "Mensagem: <br />";
    $message .= $contact_message;
-   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+   $message .= "<br /> ----- <br /> Este e-mail foi enviado do seu formulário de contato.<br />";
 
    // Set From: header
    $from =  $name . " <" . $email . ">";
@@ -50,7 +50,7 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+      else { echo "Algo deu errado. Tente novamente."; }
 		
 	} # end if - no validation error
 
